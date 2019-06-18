e814568c5a7e3cd3f447ba307b36b9ea51bff06f
Josh Eckels <jeckels@labkey.com>
2019-06-18 12:42:56 -0700

    Propagate 404 error as FileNotFoundException instead of runtime AmazonS3 (#12)

--- src/main/java/com/upplication/s3fs/attribute/S3BasicFileAttributeView.java
+++ src/main/java/com/upplication/s3fs/attribute/S3BasicFileAttributeView.java
@@ -3,16 +3,15 @@ package com.upplication.s3fs.attribute;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import com.upplication.s3fs.S3Path;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 
@@ -44,65 +43,76 @@ public class S3BasicFileAttributeView implements BasicFileAttributeView {
 
     @Override
     public void setTimes(FileTime lastModifiedTime, FileTime lastAccessTime, FileTime createTime) throws IOException {
-        AmazonS3 client = s3Path.getFileStore().getFileSystem().getClient();
-        String bucketName = s3Path.getFileStore().getBucket().getName();
-        String fileKey = s3Path.getKey();
-        ObjectMetadata metadataCopy = client.getObjectMetadata(bucketName, fileKey).clone();
-        setMetadataTimes(metadataCopy, lastModifiedTime, lastAccessTime, createTime);
+        try {
+            AmazonS3 client = s3Path.getFileStore().getFileSystem().getClient();
+            String bucketName = s3Path.getFileStore().getBucket().getName();
+            String fileKey = s3Path.getKey();
+            ObjectMetadata metadataCopy = client.getObjectMetadata(bucketName, fileKey).clone();
+            setMetadataTimes(metadataCopy, lastModifiedTime, lastAccessTime, createTime);
 
-        // S3 doesn't let you modify attributes of existing objects. However, you can set them as part of a copy
-        // operation, and the copy's source and target locations can be identical. This means we don't have to download
-        // and reupload the object, which is nice.
+            // S3 doesn't let you modify attributes of existing objects. However, you can set them as part of a copy
+            // operation, and the copy's source and target locations can be identical. This means we don't have to download
+            // and reupload the object, which is nice.
 
-        long objectSize = metadataCopy.getContentLength();
-        // S3 doesn't support single-copy requests when the object is >5GB, so use a multi-part approach when needed
-        // Taken from https://docs.aws.amazon.com/AmazonS3/latest/dev/CopyingObjctsUsingLLJavaMPUapi.html
+            long objectSize = metadataCopy.getContentLength();
+            // S3 doesn't support single-copy requests when the object is >5GB, so use a multi-part approach when needed
+            // Taken from https://docs.aws.amazon.com/AmazonS3/latest/dev/CopyingObjctsUsingLLJavaMPUapi.html
 
-        long partSize = 5L * 1024L * 1024L * 1024L - 1; // 5GB chunks
-        if (objectSize > partSize) {
+            long partSize = 5L * 1024L * 1024L * 1024L - 1; // 5GB chunks
+            if (objectSize > partSize) {
 
-            InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, fileKey).withObjectMetadata(metadataCopy);
-            InitiateMultipartUploadResult initResult = client.initiateMultipartUpload(initRequest);
+                InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, fileKey).withObjectMetadata(metadataCopy);
+                InitiateMultipartUploadResult initResult = client.initiateMultipartUpload(initRequest);
 
-            long bytePosition = 0;
-            int partNum = 1;
-            List<CopyPartResult> copyResponses = new ArrayList<>();
-            while (bytePosition < objectSize) {
-                // The last part might be smaller than partSize, so check to make sure
-                // that lastByte isn't beyond the end of the object.
-                long lastByte = Math.min(bytePosition + partSize - 1, objectSize - 1);
+                long bytePosition = 0;
+                int partNum = 1;
+                List<CopyPartResult> copyResponses = new ArrayList<>();
+                while (bytePosition < objectSize) {
+                    // The last part might be smaller than partSize, so check to make sure
+                    // that lastByte isn't beyond the end of the object.
+                    long lastByte = Math.min(bytePosition + partSize - 1, objectSize - 1);
 
-                // Copy this part.
-                CopyPartRequest copyRequest = new CopyPartRequest()
+                    // Copy this part.
+                    CopyPartRequest copyRequest = new CopyPartRequest()
+                            .withSourceBucketName(bucketName)
+                            .withSourceKey(fileKey)
+                            .withDestinationBucketName(bucketName)
+                            .withDestinationKey(fileKey)
+                            .withUploadId(initResult.getUploadId())
+                            .withFirstByte(bytePosition)
+                            .withLastByte(lastByte)
+                            .withPartNumber(partNum++);
+                    copyResponses.add(client.copyPart(copyRequest));
+                    bytePosition += partSize;
+                }
+
+                // Complete the upload request to concatenate all uploaded parts and make the copied object available.
+                CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest(
+                        bucketName,
+                        fileKey,
+                        initResult.getUploadId(),
+                        getETags(copyResponses));
+                client.completeMultipartUpload(completeRequest);
+            } else {
+                CopyObjectRequest request = new CopyObjectRequest(bucketName, fileKey, bucketName, fileKey)
                         .withSourceBucketName(bucketName)
                         .withSourceKey(fileKey)
-                        .withDestinationBucketName(bucketName)
-                        .withDestinationKey(fileKey)
-                        .withUploadId(initResult.getUploadId())
-                        .withFirstByte(bytePosition)
-                        .withLastByte(lastByte)
-                        .withPartNumber(partNum++);
-                copyResponses.add(client.copyPart(copyRequest));
-                bytePosition += partSize;
+                        .withNewObjectMetadata(metadataCopy);
+
+                client.copyObject(request);
             }
-
-            // Complete the upload request to concatenate all uploaded parts and make the copied object available.
-            CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest(
-                    bucketName,
-                    fileKey,
-                    initResult.getUploadId(),
-                    getETags(copyResponses));
-            client.completeMultipartUpload(completeRequest);
         }
-        else {
-            CopyObjectRequest request = new CopyObjectRequest(bucketName, fileKey, bucketName, fileKey)
-                    .withSourceBucketName(bucketName)
-                    .withSourceKey(fileKey)
-                    .withNewObjectMetadata(metadataCopy);
-
-            client.copyObject(request);
+        catch (AmazonS3Exception e)
+        {
+            if (e.getStatusCode() == 404)
+            {
+                FileNotFoundException notFoundException = new FileNotFoundException(e.getMessage());
+                notFoundException.initCause(e);
+                throw notFoundException;
+            }
         }
     }
+
 
     public static ObjectMetadata setMetadataTimes(ObjectMetadata metadata, FileTime lastModified, FileTime lastAccess, FileTime createTime)
     {
